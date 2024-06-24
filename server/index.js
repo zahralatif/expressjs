346c03ccec717e669c62eacb5c350ed9764ed962
zahralatif <zahralatif2107@gmail.com>
2024-06-25 01:46:05 +0400

    routes commit

--- server/index.js
+++ server/index.js
@@ -1,40 +1,17 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
-const { getProducts, getProductById, deleteProductById, createProduct, updateProductById, getProductsWithLimit, getAllCategories } = require('./controllers/productController');
-const { getCarts, getCartById, getUserById, createCart, updateCartById, deleteCartById } = require('./controllers/cartsController');
-const { getUsers, createUser, updateUserById, deleteUserById } = require('./controllers/usersController');
+// dotenv.config(); //? it shows error : Error: listen EACCES: permission denied 5000;
 
 const port = process.env.PORT || 3000;
+const productRoute = require('./routes/productRoute');
+const cartsRoute = require('./routes/cartsRoute');
+const usersRoute = require('./routes/usersRoute');
 
-app.get('/', (req, res) => {
-    res.send("Home page");
-});
-
-// app.get('/users', (req, res) => {
-//     res.send("All users");
-// });
-
-app.get('/products', getProducts);
-app.get('/products/:id', getProductById);
-app.post('/products', createProduct); 
-app.put('/products/:id', updateProductById);
-app.delete('/products/:id', deleteProductById);
-app.get('/products/limit', getProductsWithLimit); //? didn't work
-app.get('/products/categories', getAllCategories);
-app.get('/carts', getCarts);
-app.get('/carts/:id', getCartById);
-app.get('/carts/user/:id', getUserById);
-app.post('/carts', createCart); 
-app.put('/carts/:id', updateCartById);
-app.delete('/carts/:id', deleteCartById);
-app.get('/users', getUsers);
-app.get('/users/:id', getUserById);
-app.post('/users', createUser); 
-app.put('/users/:id', updateUserById);
-app.delete('/users/:id', deleteUserById);
-
-
+app.use('/api/v1/products', productRoute);
+app.use('/api/v1/carts', cartsRoute);
+app.use('/api/v1/users', usersRoute);
+ 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
