f2bb26e0ebe273bb8add2834c540c2e59cb61267
zahralatif <zahralatif2107@gmail.com>
2024-06-25 02:06:31 +0400

    ejs commit

--- server/index.js
+++ server/index.js
@@ -2,11 +2,18 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 // dotenv.config(); //? it shows error : Error: listen EACCES: permission denied 5000;
-
 const port = process.env.PORT || 3000;
 const productRoute = require('./routes/productRoute');
 const cartsRoute = require('./routes/cartsRoute');
 const usersRoute = require('./routes/usersRoute');
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Home page',
+        name: 'LatiForti'
+})})
 
 app.use('/api/v1/products', productRoute);
 app.use('/api/v1/carts', cartsRoute);
